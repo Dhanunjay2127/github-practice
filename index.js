141ce537a5e391e5fcaf63da5801458312da340e
Dhanunjay2127 <dhanunjaya@mobil80.com>
2025-02-03 18:31:20 +0530

    express route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send({ status: "Success" });
+});
+
 app.listen(3000, () => {
   console.log("server running in 3000");
 });
